259fec765b06c8077b13cb7b61401efbf75b2911
Oleh Tverdokhlib <49977018+otver2@users.noreply.github.com>
2019-06-15 17:42:43 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import User from "./components/User";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <GithubState>
+    <AlertState>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/user/:login" component={User} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </AlertState>
+  </GithubState>
+);
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import Search from "./Search";
+import Users from "./Users";
+
+const Home = () => (
+  <Fragment>
+    <Search />
+    <Users />
+  </Fragment>
+);
+
+export default Home;
